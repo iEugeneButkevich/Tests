a3ac0fa23f7192fc239368e1264ff52f0fb8d83e
Eugene <eugene@Eugenes-Mac.local>
2016-06-06 16:14:54 +0300

    First commit

---
+++ DictionaryTest/DictionaryTestTests/DictionaryTestTests.h
@@ -0,0 +1,13 @@
+//
+//  DictionaryTestTests.h
+//  DictionaryTestTests
+//
+//  Created by Eugene on 6/6/16.
+//  Copyright (c) 2016 EugeneButkevich. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DictionaryTestTests : SenTestCase
+
+@end
---
+++ DictionaryTest/DictionaryTestTests/DictionaryTestTests.m
@@ -0,0 +1,32 @@
+//
+//  DictionaryTestTests.m
+//  DictionaryTestTests
+//
+//  Created by Eugene on 6/6/16.
+//  Copyright (c) 2016 EugeneButkevich. All rights reserved.
+//
+
+#import "DictionaryTestTests.h"
+
+@implementation DictionaryTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DictionaryTestTests");
+}
+
+@end
